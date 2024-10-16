d51a1f4cd432cdfe272e073d3cab32d955a29fdf
javiersuweijie <javier.su.weijie@gmail.com>
2024-10-16 16:47:48 +0800

    fix: script to re-add all dkim entries

--- packages/app/src/scripts/write_dkim_entry.ts
+++ packages/app/src/scripts/write_dkim_entry.ts
@@ -1,10 +1,19 @@
-import { addDkimEntry } from "@/lib/contract-deploy"
+import { addDkimEntry } from "@/lib/contract-deploy";
+import { prisma } from "@/lib/prisma";
 
 (async () => {
-    await addDkimEntry({
-        parameters: {
-            senderDomain: 'github.com',
-            dkimSelector: 'pf2023'
+    const entries = await prisma.entry.findMany({
+        where: {
+            status: "COMPLETED",
+            withModal: true,
         }
-    } as any)
+    });
+    for (const entry of entries) {
+        console.log("Adding DKIM entry for entry: ", entry.slug)
+        try {
+            await addDkimEntry(entry)
+        } catch {
+            console.log("Error adding DKIM entry for: ", entry.slug);
+        }
+    }
 })()
