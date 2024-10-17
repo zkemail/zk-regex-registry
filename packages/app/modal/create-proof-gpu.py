b02cf2eb4ab3b609378912ac81e6d9c2e8540420
javiersuweijie <javier.su.weijie@gmail.com>
2024-10-17 13:06:30 +0800

    feat: use deployed modal app

--- packages/app/modal/create-proof-gpu.py
+++ packages/app/modal/create-proof-gpu.py
@@ -2,7 +2,7 @@ import modal
 import subprocess
 import os
 import sys
-app = modal.App()
+app = modal.App("create-proof-gpu")
 
 base_image = modal.Image.from_registry("javiersuweijie/prover-gpu:0.0.1-alpha.0")
 secret = modal.Secret.from_name(
@@ -17,17 +17,19 @@ bucket_name = os.environ.get("BUCKET_NAME", "")
 def main():
     slug = os.environ["PROJECT_SLUG"]
     circuitName = os.environ["CIRCUIT_NAME"]
-    proof, public = prove.remote(slug, circuitName)
-    # write return to file
-    with open(f"{path}/proof.json", "w") as f:
-        f.write(proof)
-    with open(f"{path}/public.json", "w") as f:
-        f.write(public)
+    with open(f"{path}/input.json", "r") as f:
+        prove2 = modal.Function.lookup("create-proof-gpu", "prove")
+        proof, public = prove2.remote(slug, circuitName, f.read())
+        # write return to file
+        with open(f"{path}/proof.json", "w") as f:
+            f.write(proof)
+        with open(f"{path}/public.json", "w") as f:
+            f.write(public)
 
 @app.function(
     timeout=3600,
     image=base_image,
-    cpu=16,
+    container_idle_timeout=300,
     gpu="H100",
     volumes={
         "/output": modal.CloudBucketMount(
@@ -37,15 +39,17 @@ def main():
             read_only=True
         )
     },
-    mounts=[modal.Mount.from_local_dir(path, remote_path="/proof")]
     )
-def prove(slug: str, circuitName: str):
-    subprocess.run(["node", f"/output/circuit/{slug}/{circuitName}_js/generate_witness.js", f"/output/circuit/{slug}/{circuitName}_js/{circuitName}.wasm", f"/proof/input.json", f"/proof/output.wtns"], check=True)
-    subprocess.run(["/rapidsnark/package/bin/prover_cuda", f"/output/circuit/{slug}/{circuitName}.zkey", f"/proof/output.wtns", f"/proof/proof.json", f"/proof/public.json"], check=True)
+def prove(slug: str, circuitName: str, input):
+    with open(f"input.json", "w") as f:
+        f.write(input)
+
+    subprocess.run(["node", f"/output/circuit/{slug}/{circuitName}_js/generate_witness.js", f"/output/circuit/{slug}/{circuitName}_js/{circuitName}.wasm", f"input.json", f"output.wtns"], check=True)
+    subprocess.run(["/rapidsnark/package/bin/prover_cuda", f"/output/circuit/{slug}/{circuitName}.zkey", f"output.wtns", f"proof.json", f"public.json"], check=True)
 
     # read from proof.json and public.json and return them
-    with open(f"/proof/proof.json", "r") as f:
+    with open(f"proof.json", "r") as f:
         proof = f.read()
-    with open(f"/proof/public.json", "r") as f:
+    with open(f"public.json", "r") as f:
         public = f.read()
     return proof, public
