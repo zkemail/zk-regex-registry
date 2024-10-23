f5e051543f03e21d95e64ed9410cbc5159c0399a
javiersuweijie <javier.su.weijie@gmail.com>
2024-10-23 10:23:08 +0800

    chore: removed the generate API

--- packages/app/src/workers/circuit-generator/index.ts
+++ packages/app/src/workers/circuit-generator/index.ts
@@ -4,6 +4,9 @@ import { generateCodeLibrary, shouldRetryCompilation } from "@/lib/code-gen/gen"
 import { Entry } from "@prisma/client";
 import { getLogs } from "@/lib/models/logs";
 
+const processing: string[] = [];
+let forceStop = false;
+
 async function generateCiruitService() {
     while (true) {
         let entry: Entry;
@@ -21,6 +24,7 @@ async function generateCiruitService() {
 
         const circuitName = (entry.parameters as any)['name'];
         try {
+            processing.push(entry.slug);
             // Use modal to compile circuit
             await generateCodeLibrary(entry.parameters, entry.slug, entry.status)
             await compileCircuitModal(entry.slug, circuitName, true);
@@ -36,6 +40,43 @@ async function generateCiruitService() {
                 await updateState(entry.id, "ERROR");
             }
         }
+        processing.splice(processing.indexOf(entry.slug), 1);
     }
 }
-generateCiruitService();
+
+// Function to handle graceful exit
+function handleGracefulExit() {
+    console.log('Received exit signal. Gracefully shutting down...');
+    if (forceStop) {
+        process.exit(0);
+    }
+    // Perform any cleanup tasks here if needed
+    if (processing.length > 0) {
+        console.log("Waiting for circuits to finish processing", processing);
+        console.log("Cancel again to force exit");
+        setInterval(() => {
+            console.log("Waiting for circuits to finish processing", processing);
+            console.log("Cancel again to force exit");
+        }, 10000);
+        forceStop = true;
+    } else {
+        process.exit(0);
+    }
+}
+
+// Listen for SIGINT and SIGTERM signals
+process.on('SIGINT', handleGracefulExit);
+process.on('SIGTERM', handleGracefulExit);
+
+// Wrap the main loop in a try-catch block
+async function main() {
+    try {
+        await generateCiruitService();
+    } catch (error) {
+        console.error('Unhandled error in main loop:', error);
+        process.exit(1);
+    }
+}
+
+main();
+
