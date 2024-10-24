ca2d63a4af52e75d245670b655b8311a493eea03
javiersuweijie <javier.su.weijie@gmail.com>
2024-10-24 15:02:22 +0800

    fix: unable to edit after sample email

--- packages/app/src/app/edit/[[...slug]]/content.tsx
+++ packages/app/src/app/edit/[[...slug]]/content.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { formSchema } from "@/app/submit/form";
 import { z } from "zod";
 import { submit } from "./action";
@@ -5,6 +6,9 @@ import { Entry } from "@prisma/client";
 import { EntryForm } from "@/components/entry-form";
 
 export default function Content({entry}: {entry: Entry, sampleEmail: string}) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        await submit(values)
+    }
     return (
     <div className="w-full py-20 lg:py-40">
         <div className="container mx-auto">
@@ -13,7 +17,7 @@ export default function Content({entry}: {entry: Entry, sampleEmail: string}) {
                     <h1 className="text-xl md:text-3xl tracking-tighter text-left font-extrabold">Edit pattern</h1>
                     <h1 className="text-md md:text-xl tracking-tighter text-left">{entry.title}</h1>
                     <h1 className="text-md md:text-xl tracking-tighter text-left">{entry.description}</h1>
-                    <EntryForm entry={entry} onFormSubmit={submit} />
+                    <EntryForm entry={entry} onFormSubmit={onSubmit} />
                 </div>
             </div>
         </div>
